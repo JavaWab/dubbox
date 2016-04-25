5adf234a377a6ac5430271ee453c2525f84d19ec
Wangab <909240405@qq.com>
2016-04-25 22:06:07 +0800

    修改配置

--- dubbox_westar/src/main/java/com/westar/wangab/api/impl/user/UserService.java
+++ dubbox_westar/src/main/java/com/westar/wangab/api/impl/user/UserService.java
@@ -1,9 +1,11 @@
 package com.westar.wangab.api.impl.user;
 
 import com.westar.wangab.api.intf.IUserService;
+import org.springframework.web.bind.annotation.PathVariable;
 
 /**
  * Created by Administrator on 2016/4/25.
  */
+@Path("users")
 public class UserService implements IUserService {
 }
